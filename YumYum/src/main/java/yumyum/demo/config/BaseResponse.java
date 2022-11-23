5f7502332876672290988e323c5cf60f1e38fe54
GyuJeGal <lkjdee0329@gmail.com>
2022-11-23 23:56:08 +0900

    refactor : 필드 변수 추가 및 메서드 추가 String 리스트 타입의 필드 변수 추가, 예외 발생시 실행 메서드 추가

--- YumYum/src/main/java/yumyum/demo/config/BaseResponse.java
+++ YumYum/src/main/java/yumyum/demo/config/BaseResponse.java
@@ -3,6 +3,7 @@ package yumyum.demo.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -19,6 +20,7 @@ public class BaseResponse<T> {
     private final int code;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
+    private List<String> errors;
 
     // 요청에 성공한 경우
     public BaseResponse(T result) {
@@ -26,6 +28,7 @@ public class BaseResponse<T> {
         this.message = SUCCESS.getMessage();
         this.code = SUCCESS.getCode();
         this.result = result;
+        this.errors = null;
     }
 
     // 요청에 실패한 경우
@@ -33,6 +36,11 @@ public class BaseResponse<T> {
         this.isSuccess = status.isSuccess();
         this.message = status.getMessage();
         this.code = status.getCode();
+    }
+
+    // 입력에서 예외 발생시
+    public BaseResponse(List<String> errors, BaseResponseStatus status) {
+        this(status.isSuccess(), status.getMessage(), status.getCode(), null, errors);
     }
 }
 
