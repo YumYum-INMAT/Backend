6b4271b788e801799f7b36f9dd297e8e97f3f3e0
GyuJeGal <lkjdee0329@gmail.com>
2022-11-27 02:13:29 +0900

    feat : 오늘 이건 어때요? 테이블 추가

---
+++ YumYum/src/main/java/yumyum/demo/src/restaurant/entity/TodayRecommend.java
@@ -0,0 +1,30 @@
+package yumyum.demo.src.restaurant.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import yumyum.demo.config.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "todayRecommend")
+@NoArgsConstructor
+@DynamicInsert
+public class TodayRecommend extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "today_recommend_id")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String foodName;
+
+    @Column(nullable = false)
+    private String imgUrl;
+}
