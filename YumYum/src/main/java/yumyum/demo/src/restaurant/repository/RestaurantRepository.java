cfbca937ffcf75c0876476bc4d6ee3aa6fcfd0f8
GyuJeGal <lkjdee0329@gmail.com>
2022-11-29 01:24:34 +0900

    refactor :@Repository 추가

--- YumYum/src/main/java/yumyum/demo/src/restaurant/repository/RestaurantRepository.java
+++ YumYum/src/main/java/yumyum/demo/src/restaurant/repository/RestaurantRepository.java
@@ -1,11 +1,16 @@
 package yumyum.demo.src.restaurant.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import yumyum.demo.config.Status;
+import yumyum.demo.src.restaurant.dto.RestaurantDto;
 import yumyum.demo.src.restaurant.entity.RestaurantEntity;
 
+@Repository
 public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
     Optional<RestaurantEntity> findRestaurantEntityByIdAndStatus(Long restaurantId, Status status);
-
 }
