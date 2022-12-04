8d6a9d9b77c845225afc42b662640be80268e116
GyuJeGal <lkjdee0329@gmail.com>
2022-12-04 16:53:20 +0900

    feat : 음식점 상세화면 조회를 위한 리뷰 DTO 추가

---
+++ YumYum/src/main/java/yumyum/demo/src/restaurant/dto/ReviewDto.java
@@ -0,0 +1,22 @@
+package yumyum.demo.src.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewDto {
+    private Long reviewId;
+
+    private String reviewImgUrl;
+
+    private String nickName;
+
+    private int ratingStar;
+
+    private String contents;
+}
