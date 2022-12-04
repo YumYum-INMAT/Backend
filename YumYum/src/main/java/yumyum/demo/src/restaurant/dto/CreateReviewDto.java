cd30849c11068e5e55d57a696aa473974b096f4c
GyuJeGal <lkjdee0329@gmail.com>
2022-12-04 18:11:10 +0900

    refactor : Validation 수정

--- YumYum/src/main/java/yumyum/demo/src/restaurant/dto/CreateReviewDto.java
+++ YumYum/src/main/java/yumyum/demo/src/restaurant/dto/CreateReviewDto.java
@@ -17,7 +17,7 @@ import org.hibernate.validator.constraints.Range;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateReviewDto {
-    @NotBlank(message = "평점을 입력해주세요.")
+    @NotNull(message = "평점을 입력해주세요.")
     @Range(min = 1, max = 5, message = "평점은 1이상 5이하의 정수입니다.")
     @ApiModelProperty(example = "5")
     private Integer ratingStar;
@@ -26,7 +26,7 @@ public class CreateReviewDto {
     @ApiModelProperty(example = "www.test.test")
     private String imgUrl;
 
-    @NotNull(message = "URL을 입력해주세요")
+    @NotBlank(message = "리뷰 내용을 입력해주세요")
     @Size(max = 200, message = "리뷰 내용은 최대 200자까지 입력 가능합니다.")
     @ApiModelProperty(example = "맛있어요 :)")
     private String contents;
