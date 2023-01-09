926922fc0f70c0194f427b82a7d1b198a654c282
junhyeon <junhyeon@naver.com>
2023-01-09 22:34:36 +0900

    누락된 DTO add

---
+++ YumYum/src/main/java/yumyum/demo/src/user/dto/MyReviewDto.java
@@ -0,0 +1,22 @@
+package yumyum.demo.src.user.dto;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyReviewDto {
+    private Long reviewId;
+    private String contents;
+    private String imgUrl;
+    private int ratingStar;
+    private Long restaurantId;
+    private Long userId;
+
+    private String restaurantName;
+
+    private String created_time;
+
+}
