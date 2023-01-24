26e5d1d6a91f5d6ff5c54393d33ef44ff8232210
GyuJeGal <lkjdee0329@gmail.com>
2023-01-24 22:50:14 +0900

    refactor : 회원 가입, 로그인과 같은 인증 관련 로직 분리 UserService 내의 인증 관련 로직, AuthService로 분리

--- YumYum/src/main/java/yumyum/demo/src/user/service/UserService.java
+++ YumYum/src/main/java/yumyum/demo/src/user/service/UserService.java
@@ -31,107 +31,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserJdbcTempRepository userJdbcTempRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final TokenProvider tokenProvider;
-
-
-
-    @Transactional
-    public void signup(SignUpDto signUpDto) throws BaseException {
-        //아이디 중복 체크
-        if (userRepository.findUserEntityByUsername(signUpDto.getUsername()).isPresent()) {
-            throw new BaseException(DUPLICATED_USERNAME);
-        }
-
-        //이메일 중복 체크
-        if (userRepository.findUserEntityByEmail(signUpDto.getEmail()).isPresent()) {
-            throw new BaseException(DUPLICATED_EMAIL);
-        }
-
-        //닉네임 중복 체크
-        if(userRepository.findUserEntityByNickNameAndStatus(signUpDto.getNickName(), Status.ACTIVE).isPresent()) {
-            throw new BaseException(DUPLICATED_NICKNAME);
-        }
-
-        //휴대폰 번호 중복 체크
-        if(userRepository.findUserEntityByPhoneNumberAndStatus(signUpDto.getPhoneNumber(), Status.ACTIVE).isPresent()) {
-            throw new BaseException(DUPLICATED_PHONE_NUMBER);
-        }
-
-        //빌더 패턴의 장점
-        Authority authority = Authority.builder()
-                .authorityName("ROLE_USER")
-                .build();
-
-        UserEntity user = UserEntity.builder()
-                .username(signUpDto.getUsername())
-                .password(passwordEncoder.encode(signUpDto.getPassword()))
-                .email(signUpDto.getEmail())
-                .phoneNumber(signUpDto.getPhoneNumber())
-                .nickName(signUpDto.getNickName())
-                .age(signUpDto.getAge())
-                .gender(signUpDto.getGender())
-                .authorities(Collections.singleton(authority))
-                .build();
-
-        userRepository.save(user);
-    }
-
-    //아이디 존재 여부 체크 -> 로그인에서 사용
-    public void checkUsername(String username) throws BaseException {
-        if(userRepository.findUserEntityByUsername(username).isEmpty()) {
-            throw new BaseException(FAILED_TO_LOGIN);
-        }
-    }
-
-    //아이디 중복 여부 체크
-    public void checkUsernameDuplicate(String username) throws BaseException {
-        if(userRepository.findUserEntityByUsername(username).isPresent()) {
-            throw new BaseException(DUPLICATED_USERNAME);
-        }
-    }
-
-    public void checkPassword(String username, String password) throws BaseException {
-        UserEntity user = userRepository.findUserEntityByUsernameAndStatus(username, Status.ACTIVE)
-                .orElseThrow(() -> new BaseException(NOT_ACTIVATED_USER));
-
-        if(!passwordEncoder.matches(password, user.getPassword())) {
-            throw new BaseException(FAILED_TO_LOGIN);
-        }
-
-    }
-
-    public void checkNickName(String nickName) throws BaseException {
-        if(userRepository.findUserEntityByNickNameAndStatus(nickName, Status.ACTIVE).isPresent()) {
-            throw new BaseException(DUPLICATED_NICKNAME);
-        }
-    }
-
-    public LoginDto anonymousLogin() throws BaseException {
-        List<UserEntity> anonymousUserEntities = userRepository.findAllByEmail("anonymous@email.com");
-
-        Integer anonymousUserSize = anonymousUserEntities.size() + 1;
-
-        //빌더 패턴의 장점
-        Authority authority = Authority.builder()
-                .authorityName("ROLE_ANONYMOUS")
-                .build();
-
-        UserEntity user = UserEntity.builder()
-                .username("anonymous" + Integer.toString(anonymousUserSize))
-                .password(passwordEncoder.encode("1234abcd!"))
-                .email("anonymous@email.com")
-                .phoneNumber("010-0000-0000")
-                .nickName("비회원유저")
-                .age(0)
-                .gender('M')
-                .authorities(Collections.singleton(authority))
-                .build();
-
-        userRepository.save(user);
-
-        return new LoginDto("anonymous" + Integer.toString(anonymousUserSize), "1234abcd!");
-    }
 
     public GetUserProfileDto getUserProfile(String username) throws BaseException {
         UserEntity foundUserEntity = userRepository.findUserEntityByUsernameAndStatus(username, Status.ACTIVE)
@@ -164,15 +63,17 @@ public class UserService {
         userRepository.save(foundUserEntity);
     }
 
-
+    public void checkNickName(String nickName) throws BaseException {
+        if(userRepository.findUserEntityByNickNameAndStatus(nickName, Status.ACTIVE).isPresent()) {
+            throw new BaseException(DUPLICATED_NICKNAME);
+        }
+    }
 
     public List<CommunityMainDto> getPost(String username) throws BaseException {
         Long user_id = userJdbcTempRepository.findUserIdByUsername(username);
 
         try{
-            List<CommunityMainDto> communityMainDtoList = userJdbcTempRepository.getPost(user_id);
-
-            return communityMainDtoList;
+            return userJdbcTempRepository.getPost(user_id);
         }catch (Exception exception){
             throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
         }
@@ -182,9 +83,7 @@ public class UserService {
         Long user_id = userJdbcTempRepository.findUserIdByUsername(username);
 
         try{
-            List<MyReviewDto> myReviewDtoList = userJdbcTempRepository.getMyReview(user_id);
-
-            return myReviewDtoList;
+            return userJdbcTempRepository.getMyReview(user_id);
         }catch (Exception exception){
             throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
         }
@@ -194,29 +93,10 @@ public class UserService {
         Long user_id = userJdbcTempRepository.findUserIdByUsername(username);
 
         try{
-            List<MyHeartRestaurantDto> myHeartRestaurantDtoList = userJdbcTempRepository.getMyHeartRestaurant(user_id);
-
-            return myHeartRestaurantDtoList;
+            return userJdbcTempRepository.getMyHeartRestaurant(user_id);
         }catch (Exception exception){
             throw new BaseException(DATABASE_ERROR);
         }
 
-    }
-
-    public void updateRefreshToken(String username, String refreshToken, String userAgent) throws BaseException {
-        UserEntity foundUserEntity = userRepository.findUserEntityByUsernameAndStatus(username, Status.ACTIVE)
-                .orElseThrow(() -> new BaseException(NOT_ACTIVATED_USER));
-
-        Optional<RefreshTokenEntity> refreshTokenEntity = refreshTokenRepository.findRefreshTokenEntityByUserAndStatus(foundUserEntity, Status.ACTIVE);
-
-        //첫 로그인인 경우 -> 디비에 저장된 refresh 토큰 칼럼이 없음
-        if (refreshTokenEntity.isEmpty()) {
-            RefreshTokenEntity createdRefreshTokenEntity = new RefreshTokenEntity(foundUserEntity, refreshToken, userAgent);
-            refreshTokenRepository.save(createdRefreshTokenEntity);
-        }
-        else {
-            refreshTokenEntity.get().updateRefreshToken(refreshToken);
-            refreshTokenRepository.save(refreshTokenEntity.get());
-        }
     }
 }
