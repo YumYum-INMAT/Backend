77337193069aa2a8b6ce4c8e8b458c9969c4ad22
GyuJeGal <lkjdee0329@gmail.com>
2023-02-02 16:50:41 +0900

    feat : createdAt, updatedAt과 같은 LocalDateTime을 String 값으로 전환해 커스텀하는 메서드 분리

--- YumYum/src/main/java/yumyum/demo/src/restaurant/service/RestaurantService.java
+++ YumYum/src/main/java/yumyum/demo/src/restaurant/service/RestaurantService.java
@@ -6,9 +6,6 @@ import static yumyum.demo.config.BaseResponseStatus.FAIL_TO_FIND_HEART;
 import static yumyum.demo.config.BaseResponseStatus.NOT_ACTIVATED_RESTAURANT;
 import static yumyum.demo.config.BaseResponseStatus.NOT_ACTIVATED_USER;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +28,7 @@ import yumyum.demo.src.user.entity.UserEntity;
 import yumyum.demo.src.user.repository.UserRepository;
 
 import static yumyum.demo.config.BaseResponseStatus.*;
+import static yumyum.demo.utils.ConvertUtil.convertCreatedAt;
 
 @Service
 @RequiredArgsConstructor
@@ -338,30 +336,5 @@ public class RestaurantService {
         }
 
         return result;
-    }
-
-    private String convertCreatedAt(Temporal createAt) {
-        if (ChronoUnit.YEARS.between(createAt, LocalDateTime.now()) >= 1) {
-            return Long.toString(ChronoUnit.YEARS.between(createAt, LocalDateTime.now())).concat("년 전");
-        }
-        else if (ChronoUnit.MONTHS.between(createAt, LocalDateTime.now()) >= 1) {
-            return Long.toString(ChronoUnit.MONTHS.between(createAt, LocalDateTime.now())).concat("달 전");
-        }
-        else if (ChronoUnit.WEEKS.between(createAt, LocalDateTime.now()) >= 1) {
-            return Long.toString(ChronoUnit.WEEKS.between(createAt, LocalDateTime.now())).concat("주 전");
-        }
-        else if (ChronoUnit.DAYS.between(createAt, LocalDateTime.now()) >= 1) {
-            return Long.toString(ChronoUnit.DAYS.between(createAt, LocalDateTime.now())).concat("일 전");
-        }
-        else if (ChronoUnit.HOURS.between(createAt, LocalDateTime.now()) >= 1) {
-            return Long.toString(ChronoUnit.HOURS.between(createAt, LocalDateTime.now())).concat("시간 전");
-        }
-        else if (ChronoUnit.MINUTES.between(createAt, LocalDateTime.now()) >= 1) {
-            return Long.toString(ChronoUnit.MINUTES.between(createAt, LocalDateTime.now())).concat("분 전");
-        }
-        else {
-            return Long.toString(ChronoUnit.SECONDS.between(createAt, LocalDateTime.now())).concat("초 전");
-        }
-
     }
 }
