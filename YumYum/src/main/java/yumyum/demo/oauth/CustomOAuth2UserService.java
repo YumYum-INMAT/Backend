1aa2aea08646922e775966758e156e7628c5c9a5
GyuJeGal <lkjdee0329@gmail.com>
2023-02-20 22:25:52 +0900

    refactor : 불필요한 import문 삭제 및 username 수정

--- YumYum/src/main/java/yumyum/demo/oauth/CustomOAuth2UserService.java
+++ YumYum/src/main/java/yumyum/demo/oauth/CustomOAuth2UserService.java
@@ -1,19 +1,15 @@
 package yumyum.demo.oauth;
 
-import static yumyum.demo.config.BaseResponseStatus.NOT_FOUND_OAUTH_TYPE_KAKAO;
-
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import yumyum.demo.config.BaseException;
 import yumyum.demo.config.LogInType;
-import yumyum.demo.oauth.OAuthAttributes;
 import yumyum.demo.src.user.entity.UserEntity;
 import yumyum.demo.src.user.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
